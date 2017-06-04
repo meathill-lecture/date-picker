e438b3c63bfac1ab2057284daf53efbb07ed8eea
Meathill <meathill@gmail.com>
2017-06-04 18:15:25 +0800

    先用之前写好的 EasyDate

--- date-picker/app/EasyDate.js
+++ date-picker/app/EasyDate.js
@@ -6,9 +6,21 @@ const METHODS = {
   m: 'Month',
   d: 'Date'
 };
+const defaultFormat = 'yyyy-mm-dd';
 
 export default class EasyDate {
-  constructor(offset) {
+  constructor(offset, options = {}) {
+    this.format = options.format || defaultFormat;
+    let date = EasyDate.isDate(offset, this.format);
+    if (date) {
+      this.base = new Date(date);
+      return;
+    }
+    if (offset instanceof Date) {
+      this.base = new Date(offset);
+      return;
+    }
+
     this.base = new Date();
     this.base.setHours(0);
     this.base.setMinutes(0);
@@ -30,8 +42,110 @@ export default class EasyDate {
     return this;
   }
 
+  clone() {
+    return new EasyDate(this.base, {
+      format: this.format
+    });
+  }
+
+  getDay() {
+    return this.base.getDay();
+  }
+
+  getFirstDayOfThisMonth() {
+    let date = this.clone();
+    date.setDate(1);
+    return date.getDay();
+  }
+
+  isSameMonth(date) {
+    return this.base.getFullYear() === date.getFullYear() && this.base.getMonth() === date.getMonth();
+  }
+
+  setDate(date) {
+    this.base.setDate(date);
+  }
+
   toDate() {
     return this.base;
+  }
+
+  toObject(today, start, end) {
+    let month = this.base.getMonth();
+    return {
+      year: this.base.getFullYear(),
+      month: EasyDate.toDouble(month + 1),
+      empty: this.getFirstDayOfThisMonth(),
+      days: EasyDate.getDates(this.base, today, start, end, this.format)
+    };
+  }
+
+  toString() {
+    return EasyDate.format(this.base, this.format);
+  }
+
+  static format(date, format) {
+    return format
+      .replace(/y+/gi, () => {
+        return date.getFullYear();
+      })
+      .replace(/m+/gi, () => {
+        return EasyDate.toDouble(date.getMonth() + 1);
+      })
+      .replace(/d+/gi, () => {
+        return EasyDate.toDouble(date.getDate());
+      });
+  }
+
+  static getDates(date, today, start, end, format = defaultFormat) {
+    let month = date.getMonth();
+    date = new Date(date);
+    date.setDate(1);
+    let dates = [];
+    while (date.getMonth() === month) {
+      let label = EasyDate.format(date, format);
+      dates.push({
+        date: label.substr(0, 10),
+        today: today && today.toString() === label,
+        disabled: (start && label < start.toString()) || (end && label > end.toString())
+      });
+      date.setDate(date.getDate() + 1);
+    }
+    return dates;
+  }
+
+  static isDate(string, format) {
+    format = format || defaultFormat;
+    string = string.toString();
+    let pos = [];
+    let regexps = [/d+/gi, /y+/gi, /m+/gi];
+    let origin = format;
+    regexps.forEach( regexp => {
+      format = format.replace(regexp, match => {
+        pos.push(match.substr(0, 1));
+        return '(\\d{' + match.length + '})';
+      });
+    });
+    let regexp = new RegExp(`^${format}$`);
+    let check = string.match(regexp);
+    if (!check) {
+      return check;
+    }
+    let result = { };
+    ['y', 'm', 'd'].forEach(key => {
+      let regexp = new RegExp(`${key}+`, 'gi');
+      origin.replace(regexp, (match, i) => {
+        result[key] = string.substr(i, match.length);
+      });
+    });
+    return `${result.y}-${result.m}-${result.d}`;
+  }
+
+  static isLeapYear(year) {
+    if (year % 100 === 0) {
+      return year % 400 === 0;
+    }
+    return year % 4 === 0;
   }
 
   static parse(offset) {
@@ -45,5 +159,9 @@ export default class EasyDate {
       result[unit] = Number(number);
     });
     return result;
+  }
+
+  static toDouble(number) {
+    return number > 9 ? number.toString() : ('0' + number);
   }
 }
