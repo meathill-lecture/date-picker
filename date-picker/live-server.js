8624ace564df50192cce2909c54c274becd46e23
meathill <meathill@gmail.com>
2018-04-05 19:04:51 +0800

    Change: add dev script.

---
+++ date-picker/live-server.js
@@ -0,0 +1,12 @@
+const liveServer = require('live-server');
+
+var params = {
+  port: 9002, // Set the server port. Defaults to 8080.
+  host: "localhost", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+  root: "./", // Set root directory that's being served. Defaults to cwd.
+  open: true, // When false, it won't load your browser by default.
+  file: "demo.html", // When set, serve this file for every 404 (useful for single-page applications)
+  wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+  logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+};
+liveServer.start(params);
